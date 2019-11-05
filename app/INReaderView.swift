fa3b474dc2df95d4cfc2e8e3767df1f3ed81c988
MIYAZAKI Hiromu <miyazaki@osstech.co.jp>
2019-11-05 19:05:03 +0900

    update label

--- app/INReaderView.swift
+++ app/INReaderView.swift
@@ -22,7 +22,7 @@ class INReaderView: UIView {
                            + "読み取り開始ボタンを押下後、端末をカードにかざしてください。"
 
         let pinLabel = CustomViewUtil.createTextView(UIScreen.main.bounds.size)
-        pinLabel.text = "暗証番号(4桁)"
+        pinLabel.text = "暗証番号(4桁、必須)"
 
         pinField = CustomViewUtil.createTextField(UIScreen.main.bounds.size)
         pinField.isSecureTextEntry = true
