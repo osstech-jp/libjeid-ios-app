db21a512dfffd6daf1a782dfc512f14337565457
HAMANO Tsukasa <hamano@osstech.co.jp>
2019-11-27 19:19:15 +0900

    refactoring: update-select-identifiers.sh

--- update-select-identifiers.sh
+++ update-select-identifiers.sh
@@ -1,12 +1,19 @@
-#!/bin/sh
+#!/bin/bash
 
 INFO_PLIST=$1
 
-/usr/libexec/PlistBuddy -c 'delete :com.apple.developer.nfc.readersession.iso7816.select-identifiers' ${INFO_PLIST}
-/usr/libexec/PlistBuddy -c 'add :com.apple.developer.nfc.readersession.iso7816.select-identifiers array' ${INFO_PLIST}
-/usr/libexec/PlistBuddy -c 'add :com.apple.developer.nfc.readersession.iso7816.select-identifiers: string A00000023101' ${INFO_PLIST}
-/usr/libexec/PlistBuddy -c 'add :com.apple.developer.nfc.readersession.iso7816.select-identifiers: string A00000023102' ${INFO_PLIST}
-/usr/libexec/PlistBuddy -c 'add :com.apple.developer.nfc.readersession.iso7816.select-identifiers: string D392F000260100000001' ${INFO_PLIST}
-/usr/libexec/PlistBuddy -c 'add :com.apple.developer.nfc.readersession.iso7816.select-identifiers: string D3921000310001010402' ${INFO_PLIST}
-/usr/libexec/PlistBuddy -c 'add :com.apple.developer.nfc.readersession.iso7816.select-identifiers: string D3921000310001010408' ${INFO_PLIST}
+ENTRY="com.apple.developer.nfc.readersession.iso7816.select-identifiers"
+SELECT_IDENTIFIERS=(
+    "A00000023101"
+    "A00000023102"
+    "D392F000260100000001"
+    "D3921000310001010402"
+    "D3921000310001010408"
+)
+
+/usr/libexec/PlistBuddy -c "delete :${ENTRY}" ${INFO_PLIST}
+/usr/libexec/PlistBuddy -c "add :${ENTRY} array" ${INFO_PLIST}
+for aid in "${SELECT_IDENTIFIERS[@]}" ; do
+    /usr/libexec/PlistBuddy -c "add :${ENTRY}: string ${aid}" ${INFO_PLIST}
+done
 
